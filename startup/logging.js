78e6150e29469dfb26839ec30b8fd1e2c0be8b6c
Erasmo <erasmosaraujo@gmail.com>
2020-11-02 00:06:43 -0500

    Fix

--- startup/logging.js
+++ startup/logging.js
@@ -7,14 +7,14 @@ const config = require('config');
 
 module.exports = function () {
 
-    if (!config.get('connectionString-log')) {
-        console.error('FATAL ERROR: connectionString-log is not defined');
+    if (!config.get('connectionStringLog')) {
+        console.error('FATAL ERROR: connectionStringLog is not defined');
         process.exit(1);
     }
 
     winston.add(new winston.transports.Console({ colorize: true, prettyPrint: true }));
     winston.add(new winston.transports.File({ filename: 'logfile.log', handleExceptions: true }));
-    winston.add(new winston.transports.MongoDB({ db: config.get('connectionString-log') }));
+    winston.add(new winston.transports.MongoDB({ db: config.get('connectionStringLog') }));
 
     process.on('unhandledRejection', (ex) => {
         throw ex;
