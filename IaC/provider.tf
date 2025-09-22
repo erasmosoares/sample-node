31fed1e460af25161a0a63b26d4968c6597ac35e
Erasmo <erasmosaraujo@gmail.com>
2025-09-21 23:38:43 -0400

    Testing

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -6,12 +6,4 @@ terraform {
       name = "Node-Template"
     }
   }
-
-  required_version = ">= 1.0.0"
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
 }
